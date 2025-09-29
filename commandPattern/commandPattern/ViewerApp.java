9b9eb989112a587fb7266484e9ee2a010a318a4a
Angelo Cruz <angelojoseph024@gmail.com>
2025-09-29 11:55:30 +0800

    ViewerApp.java

--- commandPattern/commandPattern/ViewerApp.java
+++ commandPattern/commandPattern/ViewerApp.java
@@ -5,21 +5,21 @@ public class ViewerApp {
         RemoteControl rc = new RemoteControl();
 
         // Control TV
-        Device tv = new Tv();
+        Device tv = (Device) new Tv();
         rc.setCommand(new PowerOn(tv));
         System.out.println(rc.pressButton());
         rc.setCommand(new PowerOff(tv));
         System.out.println(rc.pressButton());
 
         // Control Stereo
-        Device stereo = new Stereo();
+        Device stereo = (Device) new Stereo();
         rc.setCommand(new PowerOn(stereo));
         System.out.println(rc.pressButton());
         rc.setCommand(new PowerOff(stereo));
         System.out.println(rc.pressButton());
 
         // Control Light
-        Device light = new Light();
+        Device light = (Device) new Light();
         rc.setCommand(new PowerOn(light));
         System.out.println(rc.pressButton());
         rc.setCommand(new PowerOff(light));
